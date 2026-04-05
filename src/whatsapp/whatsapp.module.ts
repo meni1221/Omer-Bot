ca843096a45592d13f9de66abe735a42787e4684
meni_levi <1224tap@gmail.com>
2026-04-06 02:12:35 +0300

    Add OmerService to WhatsappModule providers for enhanced functionality

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
+import { OmerService } from 'src/omer/omer.service';
 
 @Module({
-  providers: [WhatsappService],
+  providers: [WhatsappService, OmerService],
   exports: [WhatsappService],
 })
 export class WhatsappModule {}
