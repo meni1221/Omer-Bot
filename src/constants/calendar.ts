ada9a0ebc5fdf40ee35fb783599d0c9b31835b87
meni_levi <1224tap@gmail.com>
2026-04-07 13:34:58 +0300

    rename a file to calender

---
+++ src/constants/calendar.ts
@@ -0,0 +1,12 @@
+export const HOLIDAY_EVES_2026 = [
+  '2026-04-07', // ערב שביעי של פסח (היום!)
+  '2026-04-13', // ערב אסרו חג
+  '2026-05-21', // ערב שבועות
+];
+
+export const CONFIG = {
+  EARLY_SEND_TIME: '17:45', // זמן שליחה מוקדמת (שישי/ערב חג) 
+  SHABBAT_PROTECTION_TIME: '19:00',
+  OWNER_NUMBER: '972533011599@c.us',
+  STARTUP_PULSE_MINUTES: 10,
+};
